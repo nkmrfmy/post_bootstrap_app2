f5913b4969784d44c818b6f88b9db0ad19d39434
FMY <n.fmy.0526@icloud.com>
2022-08-17 18:12:43 +0900

    新規投稿機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,22 @@
 class PostsController < ApplicationController
-  def index
-  end
+  def index; end
 
-  def show
-  end
+  def show; end
 
   def new
+    @post = Post.new
   end
 
   def create
+    post = Post.create!(post_params)
+    redirect_to post_path(post)
   end
 
-  def edit
-  end
+  def edit; end
 
-  def update
-  end
+  def update; end
 
-  def destroy
-  end
+  def destroy; end
+
+  params.require(:post).permit(:title, :content)
 end
